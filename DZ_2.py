c619caf0898373d81f5453a36fe5c972ae866ea2
Lera <Valeriya249201@yandex.ru>
2021-01-11 19:50:19 +0300

    p4 done

--- DZ_2.py
+++ DZ_2.py
@@ -37,8 +37,9 @@ for n in Array :
 for ind, n in enumerate(Array):
     print(str(ind)+" "+str(n))
 """ 
-
-Array = [1,2,3,"bebebe",5,6,"besit","hren'",[9],[8],[7]]
+"""
+inp = input("Enter nums via space\n")
+Array = inp.split(" ")
 for a, b in enumerate(Array):
     if a == len(Array) - 1:
         break
@@ -46,7 +47,10 @@ for a, b in enumerate(Array):
         Array[a] = Array[a+1]
         Array[a+1] = b
         print (str(Array[a]) + " " + str(Array[a+1]))
-print(str(Array[len(Array)-1]))  
+print(str(Array[len(Array)-1]))
+print("Result " + str(Array))  
+
+"""
 """
 arr = [10,20]
 buff = arr[a] #buff = 10
@@ -54,6 +58,68 @@ arr[a] = arr[a+1] # [20,20]
 arr[a+1] = buff # [20,10]
 print(arr)
 """
- 
-  
+"""
+inp = int(input("Enter a month\n"))
+inp = inp - 1
+Array = ["зима","зима",
+         "весна","весна","весна",
+         "лето","лето","лето",
+         "осень","осень","осень","зима"]
+print(str(Array[inp]))
+"""
+"""
+inp = input("Enter a month\n")
+dictt = {
+        "0" : "zima",
+        "1" : "vesna"
+        }
+print(dictt[inp])
+"""
+"""
+char word[10] = ["W", "i", "n"]
+string word = "Win"
+"""
+"""
+word = "456789"
+print(word[3:])
+"""
+"""
+inp = input("enter two words\n")
+inp=inp.split(" ")
+for a,d in enumerate(inp):
+    print(str(a) + " " + str(d[:10]))
+"""
+"""
+inp = input("enter two words\n")
+inp=inp.split(" ")
+l=len(inp)
+i=0
+while i < l:
+    print(str(i) + " " + str(inp[i][:10]))
+    i=i+1
+"""
+
+class dog():
+    def __init__(self, dog_type):
+        self.Type = dog_type
+        print("The " + self.Type + " dog initiated")
+    Type = "Taksa"
+    color = "blue"
+    def shout(self):
+        print("I'm " + self.Type + " and shouting loud with my color " + self.color)
+
+
+
+belka = dog("Korol")
+print(belka.Type)
+belka.Type = "Layka"
+print(belka.Type)
+belka.shout()
+
     
+    
+    
+    
+
+
+
